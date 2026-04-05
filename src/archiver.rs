8db4c67ed056d2073f4c05f4d1e6d4a88f35a5da
Direwolfesp <alvarolg365@gmail.com>
2026-04-05 02:39:39 +0200

    parsing all headers seem to work

--- src/archiver.rs
+++ src/archiver.rs
@@ -25,7 +25,7 @@ pub struct Archiver {
 }
 
 impl Archiver {
-    // TODO: keep parsing files and handle EOF
+    /// Opens the Tar archive file and parses all file objects.
     pub fn parse(filename: &Path) -> Self {
         let mut source = File::open(filename).expect("cannot open archive file");
         let mut files: Vec<FileInfo> = Vec::new();
@@ -38,16 +38,38 @@ impl Archiver {
                 .expect("Malformed tar archive");
 
             let Ok(header) = Header::parse(&record_buf) else {
+                // TODO: error handling
                 break;
             };
 
+            // save old offset
             let offset = source.stream_position().unwrap();
+
+            // skip file content
+            source
+                .seek_relative(header.file_size() as i64)
+                .expect("seek failed");
+
+            let pos = source.stream_position().unwrap();
+            let rem = pos as usize % RECORD_SIZE;
+
+            // only align forward if we are not in a position divisible by
+            // RECORD_SIZE
+            if rem != 0 {
+                let align_forward = RECORD_SIZE - rem;
+                source
+                    .seek_relative(align_forward as i64)
+                    .expect("align forward failed");
+            }
+
             files.push(FileInfo { header, offset });
         }
 
         Self { files, source }
     }
 
+    /// Pretty print all files contained in the archive in the form of
+    /// table.
     pub fn print_files(&self) {
         let mut build = Builder::default();
         build.push_record(["#", "name", "type", "size", "mode", "modified"]);
@@ -64,12 +86,15 @@ impl Archiver {
         }
 
         let mut table = build.build();
+        // little hack to make it look like nushell's one <3
         table
-            .with(Style::modern_rounded())
-            .modify(Columns::first(), Color::FG_GREEN)
+            .with(Style::rounded())
+            .modify(Columns::first(), Color::FG_GREEN | Color::BOLD)
             .modify(Columns::one(1), Color::FG_GREEN)
             .modify(Columns::one(3), Color::FG_CYAN)
             .modify(Columns::one(4), Color::FG_MAGENTA)
+            .modify(Columns::one(3), Alignment::right())
+            .modify(Columns::one(4), Alignment::right())
             .modify(Rows::first(), Color::FG_GREEN | Color::BOLD)
             .modify(Rows::first(), Alignment::center());
 
